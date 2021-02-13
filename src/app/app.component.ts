541cb78d57b333594d13dc7f89edd7262c749b86
Tahmid <tahmidfarhan12@gmail.com>
2021-02-13 16:25:08 +0600

    Email Client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+// https://api.angular-email.com/auth/signedin
+/*
+  {
+      "username" : "tahmid12" ,
+      "password" : "1234" ,
+      "passwordConfirmation" : "1234"
+  }
+ */
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'emailclient';
+
+  title = 'emailclient' ;
+
 }
